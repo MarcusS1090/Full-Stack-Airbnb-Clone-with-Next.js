7f414ad924dc7ccea591034a056638c306f88305
Marcus Senju <thegladus323@gmail.com>
2023-05-05 17:28:00 -0500

    creamos la ventana de favoritos

---
+++ app/favorites/FavoritesClient.tsx
@@ -0,0 +1,46 @@
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
+import { SafeListings, SafeUser } from "../types"
+
+interface FavoritesClientProps {
+    listings: SafeListings[];
+    currentUser?: SafeUser | null; 
+}
+
+
+const FavoritesClient: React.FC<FavoritesClientProps> = ({
+    listings,
+    currentUser
+}) => {
+    return (
+        <Container>
+            <Heading 
+                title="Favorites"
+                subtitle="List of places you have favorited!"
+            />
+            <div
+                className="
+                    mt-10
+                    grid
+                    grid-cols-1
+                    md:grid-cols-2
+                    lg:grid-cols-3
+                    xl:grid-cols-4
+                    2xl:grid-cols-6
+                    gap-8
+                "
+            >
+                {listings.map((listing) => (
+                    <ListingCard
+                        key={listing.id}
+                        currentUser={currentUser}
+                        data={listing}
+                    />
+                ))}
+            </div>
+        </Container>
+    )
+}
+
+export default FavoritesClient
